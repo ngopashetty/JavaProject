c769401ba89b880ef87ba1efb0fb3b93b5e17cb8
ngopashetty@yahoo.com <ngopashetty@yahoo.com>
2020-03-17 12:45:46 +0530

    commit3

--- OneProject/src/jenkins/project/DBConnect.java
+++ OneProject/src/jenkins/project/DBConnect.java
@@ -7,7 +7,7 @@ public class DBConnect {
 	public static void main(String[] args) {
 		//Sample
 		System.out.println("This is the class to connect to Database");
-
+		System.out.println("This is a second line added in the project");
 	}
 
 }
